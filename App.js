fedbd165dcd8cec80bc992f499b93b7ac0ccdd2a
Garvit Singh <garvitsingh4575@gmail.com>
2020-11-01 11:53:27 +0400

    💪 Initial Commit 💪

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import Quiz from "./src/Quiz";
+import Reminder from "./src/Reminder";
+import Score from "./src/Score";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Stack = createStackNavigator();
+
+export default () => (
+    <NavigationContainer>
+        <Stack.Navigator>
+            <Stack.Screen
+                name="Reminder"
+                component={Reminder}
+                options={() => ({ headerShown: false })}
+            />
+            <Stack.Screen
+                name="Quiz"
+                component={Quiz}
+                options={() => ({ headerShown: false })}
+            />
+            <Stack.Screen
+                name="Score"
+                component={Score}
+                options={() => ({ headerShown: false })}
+            />
+        </Stack.Navigator>
+    </NavigationContainer>
+);
